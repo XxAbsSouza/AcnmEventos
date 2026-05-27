a99bdcf31f037af52be759e2e51a00ca2139c0df
XxAsSouza <beatriz_ana58@hotmail.com>
2026-05-27 11:45:53 -0300

    feat: add user and client entities

---
+++ acnm-financeiro/src/main/java/br/org/acnm/model/entity/Client.java
@@ -0,0 +1,24 @@
+package br.org.acnm.model.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "client")
+public class Client extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    public UUID id;
+    
+    @OneToOne(optional = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", unique = true) 
+    public AppUser appUser; 
+
+    @Column(name = "first_name") 
+    public String firstName;
+    @Column(name = "last_name") 
+    public String lastName;
+
+    public String phone;
+}
