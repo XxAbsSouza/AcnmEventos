a0a04113ddd5aac32f0607eba2a473ae440387a8
XxAsSouza <beatriz_ana58@hotmail.com>
2026-05-30 14:10:58 -0300

    refactor

--- acnm-financeiro/src/main/java/br/org/acnm/aplication/dto/products/CategoryRequestDTO.java
+++ acnm-financeiro/src/main/java/br/org/acnm/aplication/dto/products/CategoryRequestDTO.java
@@ -4,5 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 
 public record CategoryRequestDTO(
     @NotBlank(message = "O nome da categoria é obrigatório")
-    String name
+    String name,
+
+    Integer parentId
 ) {}
